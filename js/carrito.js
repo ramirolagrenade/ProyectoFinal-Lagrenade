43c4b7471cd7ce2b194dcd40790e686918dc04ff
Ramiro Lagrenade <ramirolagrenade@gmail.com>
2023-02-23 18:00:59 -0300

    Se sube el proyecto

--- js/carrito.js
+++ js/carrito.js
@@ -1,6 +1,8 @@
 let carrito = []
 const productos = []
 
+// Se utiliza el fetch para pasar los objetos que se encuentran en el archivo JSON y trasladarlo a un array.
+
 fetch('./stock/stock.json')  
     .then((resp) => resp.json())
     .then((data) => obtenerStock(data))
@@ -11,6 +13,8 @@ const obtenerStock = (data) =>{
 }
 
 const prodContenedor = document.getElementById('producto-contenedor')
+
+// Se crea un evento click para agregar al carrito si el elemento clickeado posee la clase 'sumar', y este elemento posee un id que se utiliza para agregar un elemento determinado al carrito.
 
 prodContenedor.addEventListener('click', (e) => {
     if (e.target.classList.contains('sumar')) {
@@ -27,22 +31,25 @@ prodContenedor.addEventListener('click', (e) => {
     }
 })
 
+// En esta función recibe el Id del producto a agregar al carrito, con el find recorre los objetos dentro del carrito y si no se encuentra el mismo id se lo agrega, de lo contrario si la propiedad id coincida aumenta en 1 la propiedad de cantidad.
+
 const validarProductoEnCarrito = (productoId) => {
     const productoRepetido = carrito.find(producto => producto.id == productoId)
+    const producto = productos.filter(producto => producto.id == productoId)
 
     if (!productoRepetido) {
-        const producto = productos.find(producto => producto.id == productoId)
-        carrito.push(producto)
-        mostrarProducto(producto)
+        carrito.push(producto[0])
+        mostrarProducto(producto[0])
         actualizarTotalesCarrito(carrito)
     } else {
-        productoRepetido.cantidad++
-        const cantidadProducto = document.getElementById(`cantidad${productoRepetido.id}`)
-        cantidadProducto.innerText = `Cantidad: ${productoRepetido.cantidad}`
+        producto[0].cantidad++
+    
         actualizarTotalesCarrito(carrito)
+        mostrarProducto2(carrito)
     }
-
 }
+
+// Esta función permite visualizar los productos que se encuentras dentro del array de objetos de 'carrito' y añade el botón 'boton-eliminar' que permite eliminar un producto anteriormente añadido al carrito.
 
 const mostrarProducto = (producto) => {
     const contenedor = document.getElementById("carrito-contenedor")
@@ -50,13 +57,15 @@ const mostrarProducto = (producto) => {
     div.classList.add("productoEnCarrito")
     div.innerHTML = `
         <p>${producto.nombre}</p>
-        <p>Precio: ${producto.precio}</p>
+        <p>Precio:$ ${producto.precio}</p>
         <p id=cantidad${producto.id}>Cantidad: ${producto.cantidad}</p>
         <button class="btn waves-effect waves-ligth boton-eliminar" value="${producto.id}">X</button>
     `
     contenedor.appendChild(div)
     actualizarTotalesCarrito(carrito)
 }
+
+// Esta función actualiza el DOM luego de eliminar un producto del carrito.
 
 const mostrarProducto2 = (carrito) => {
     const contenedor = document.getElementById("carrito-contenedor")
@@ -68,13 +77,23 @@ const mostrarProducto2 = (carrito) => {
         div.classList.add("productoEnCarrito")
         div.innerHTML = `
             <p>${producto.nombre}</p>
-            <p>Precio: ${producto.precio}</p>
+            <p>Precio:$ ${producto.precio}</p>
             <p id=cantidad${producto.id}>Cantidad: ${producto.cantidad}</p>
             <button class="btn waves-effect waves-ligth boton-eliminar" value="${producto.id}">X</button>
         `
         contenedor.appendChild(div)
     })
 }
+
+// Luego de finalizar la compra con esta función elimina todos los productos del DOM del carrito.
+
+const mostrarProducto3 = () => {
+    const contenedor = document.getElementById("carrito-contenedor")
+
+    contenedor.innerHTML = ""
+}
+
+// Atreves de un evento click esta función elimina un producto del carrito, en caso de ser mayor a 1 lo decrece en 1 y en caso contrario (<=1) lo elimina del array de objetos del carrito.
 
 const eliminarProductoCarrito = (productoId) => {
 
@@ -103,14 +122,16 @@ const eliminarProductoCarrito = (productoId) => {
             }
         }).showToast()
     }
-
+    
     mostrarProducto2(carrito)
     actualizarTotalesCarrito(carrito)
 }
 
+// Esta función atreves de la función reduce suma todos los valores de la propiedad cantidad para actualizar la cantidad de productos en el carrito y realiza la ecuación de las propiedades precio * cantidad para luego sumarlas todas y multiplicarlo por el Iva.
+
 const actualizarTotalesCarrito = (carrito) => {
 
-    const totalCantidad = carrito.reduce((acc, item) => acc + item.cantidad, 0)
+    const totalCantidad = carrito.reduce((acc,item) =>acc + item.cantidad,0)
 
     const totalCompra = carrito.reduce((acc, item) => acc + (item.precio * item.cantidad), 0)
 
@@ -120,6 +141,8 @@ const actualizarTotalesCarrito = (carrito) => {
     guardarCarritoStorage(carrito)
 }
 
+// Esta función permite visualizar en el DOM tanto la cantidad de productos dentro del carrito como la sumatoria total de los productos mas iva dentro del carrito.
+
 const mostrarTotalCarrito = (totalCantidad, totalCompra) => {
     const contadorCarrito = document.getElementById("contador-carrito")
     const precioTotal = document.getElementById("precioTotal")
@@ -128,14 +151,30 @@ const mostrarTotalCarrito = (totalCantidad, totalCompra) => {
     precioTotal.innerText = totalCompra
 }
 
+// Esta función luego de finalizar la compra reestablece los valores a 0 tanto de la cantidad de productos dentro del carrito y el total de la sumatoria de los precios del carrito mas iva.
+
+const mostrarTotalCarrito2 = () => {
+    const contadorCarrito = document.getElementById("contador-carrito")
+    const precioTotal = document.getElementById("precioTotal")
+    
+    contadorCarrito.innerText = 0
+    precioTotal.innerText = 0
+}
+
+// Esta función guarda el  array de objetos del 'carrito' al localStorage.
+
 const guardarCarritoStorage = (carritoDeCompras) => {
     localStorage.setItem("carrito", JSON.stringify(carritoDeCompras))
 }
+
+// Esta función carga si hay algo guardado dentro del localStorage.
 
 const obtenerCarritoStorage = () => {
     const carritoStorage = JSON.parse(localStorage.getItem("carrito"))
     return carritoStorage
 }
+
+// Esta función atreves de un if verifica si hay algo almacenado en el localStorage y en caso de ser así los muestra dentro del carrito y actualiza la cantidad de productos dentro del carrito y el precio total de la sumatoria de productos mas iva.
 
 const cargarCarrito = () => {
     if (localStorage.getItem("carrito")) {
@@ -143,4 +182,26 @@ const cargarCarrito = () => {
         mostrarProducto2(carrito)
         actualizarTotalesCarrito(carrito)
     }
+}
+
+// Al finalizar la Compra se vacía tanto el carrito como el local storage y se le modifica la propiedad de 'cantidad' a los objetos del carrito.
+
+const finalizarCompra = (carrito) =>{
+    if (carrito.length>0){
+        Swal.fire({
+            icon: 'success',
+            title: 'Compra Realizada',
+            text: 'Su compra se realizo correctamente.',
+            background: '#0d2033',
+            color: '#fff'
+        })
+    }
+
+    productos.map(item => item.cantidad = 1) 
+    
+    carrito.splice(0,carrito.length)
+    localStorage.clear()
+
+    mostrarProducto3()
+    mostrarTotalCarrito2()
 }
--- js/eventList.js
+++ js/eventList.js
@@ -1,25 +1,28 @@
-const menuCarrito = document.querySelector('.modal-contenedor');
-const abrirCarrito = document.getElementById('cesta-carrito');
+const menuCarrito = document.querySelector('.modal-contenedor')
+const abrirCarrito = document.getElementById('cesta-carrito')
 
 abrirCarrito.addEventListener('click', () => {
     menuCarrito.classList.toggle('modal-active')
-});
+})
 
-const cerrarCarrito = document.getElementById('btn-cerrar-carrito');
+const cerrarCarrito = document.getElementById('btn-cerrar-carrito')
 
 cerrarCarrito.addEventListener('click', () => {
     menuCarrito.classList.toggle('modal-active')
-});
+})
 
 menuCarrito.addEventListener('click', () => {
     cerrarCarrito.click()
-});
+})
 
 const actualizarCarrito = document.querySelector('.modal-carrito')
 
 actualizarCarrito.addEventListener("click", (e) => {
-    e.stopPropagation();
+    e.stopPropagation()
     if (e.target.classList.contains("boton-eliminar")) {
-        eliminarProductoCarrito(e.target.value);
-    };
-});
+        eliminarProductoCarrito(e.target.value)
+    }
+    if(e.target.classList.contains("boton-compra")){
+        finalizarCompra(carrito)
+    }
+})
